4004e5098f06e28d53d7310a657a68e8e90792d5
Kazu Yamamoto <kazu@iij.ad.jp>
2012-07-09 10:16:21 +0900

    cleaning up code.

--- DB.hs
+++ DB.hs
@@ -1,37 +1,24 @@
-{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeSynonymInstances, FlexibleInstances, TypeFamilies, OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances, TypeFamilies, OverloadedStrings #-}
 {-# LANGUAGE GADTs, FlexibleContexts #-}
 module DB where
 
 import Control.Monad.Trans
-import Control.Applicative ((<$>), (<*>), empty)
-import Data.Aeson ((.:), (.=), FromJSON, ToJSON)
-import qualified Data.Aeson as A
+import Data.Text (Text)
 import Database.Persist
 import Database.Persist.Sqlite
 import Database.Persist.TH
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
-Spot
+Spot json
     lat Double
     lng Double
     body String
     deriving Show
 |]
 
-instance FromJSON Spot where
-  parseJSON (A.Object v) = Spot <$>
-                         v .: "lat" <*>
-                         v .: "lng" <*>
-                         v .: "body"
-  parseJSON _          = empty
+runDB :: MonadIO m => SqlPersist IO a -> m a
+runDB = runDB' "development.sqlite"
 
-instance ToJSON Spot where
-  toJSON (Spot lat lng body ) = A.object [ "lat" .= lat,
-                                           "lng" .= lng,
-                                           "body".= body ]
-
-runDB action = runDB' "development.sqlite" action
-
-runDB' name action = liftIO $ do
-    withSqliteConn name $ \c -> do
-    runSqlConn action c
+runDB' :: MonadIO m => Text -> SqlPersist IO a -> m a
+runDB' name action = liftIO $ withSqliteConn name $ runSqlConn action
