acc8f8ecb4c39e13b6c128a1bc3a0d8cd9a23e3a
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-07-15 19:50:16 +0900

    Add getConfig to read Rails style configuration file

---
+++ Config.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Config
+    ( getConfig
+    ) where
+
+import Control.Applicative
+import Data.Text
+import Data.Yaml
+import Data.Maybe (fromMaybe)
+import qualified Data.HashMap.Strict as M
+
+{-
+  most code is taken from Yesod's configuration handling.
+  see original in:
+    https://github.com/yesodweb/yesod/blob/master/yesod-default/Yesod/Default/Config.hs
+-}
+
+
+getConfig :: FilePath -> Text -> Text -> IO Text
+getConfig path env configName = do
+    file <- decodeFile path
+    allConfigs <- maybe (fail "Invalid YAML file") return file
+    configs <- getObject env allConfigs
+
+    lookupScalar configName configs
+
+    where
+        lookupScalar k m =
+            case M.lookup k m of
+                Just (String t) -> return t
+                Just _          -> fail $ "Invalid value for: " ++ show k
+                Nothing         -> fail $ "Not found: " ++ show k
+
+        getObject env v = do
+          envs <- fromObject v
+          maybe
+              (error $ "Could not find environment: " ++ show env)
+              return $ fromObject =<< M.lookup env envs
+
+        fromObject m =
+          case m of
+              Object o -> return o
+              _        -> fail "Invalid JSON format"
