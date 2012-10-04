470851bc4aaa13e0c19501ecdfb75883e14fb5bf
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-10-04 08:56:54 -0700

    Add DB cleanup for specs

--- Spec/Api.hs
+++ Spec/Api.hs
@@ -3,6 +3,7 @@
 
 module Spec.Api ( spec ) where
 
+import           Control.Exception       (finally)
 import qualified Data.Aeson              as AE
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Char8   as BSC8
@@ -14,21 +15,17 @@ import           System.IO
 
 spec :: P.ConnectionPool -> Spec
 spec p = do
-  let withData = bracket_ setup teardown
-           where
-             setup =
-               runDB p $ P.insert $ Spot 1.2 1.3 "ABCDE"
-             teardown =
-               runDB p $ P.deleteWhere ([] :: [P.Filter Spot])
+  let cleanup = finally $ runDB p $ P.deleteWhere ([] :: [P.Filter Spot])
 
   describe "GET /spots" $
-    it "should contains 'ABCDE' in response body" $ withData $ do
+    it "should contains 'ABCDE' in response body" $ cleanup $ do
+      runDB p $ P.insert $ Spot 1.2 1.3 "ABCDE"
       app <- getApp p
       res <- app `get` "spots"
       getBody res `shouldContains` "ABCDE"
 
   describe "GET /spots/:id" $
-    it "should get record" $ do
+    it "should get record" $ cleanup $ do
       key      <- runDB p $ P.insert (Spot 1.2 1.3 "FOO")
       resource <- runDB p $ P.get key
       app      <- getApp p
@@ -36,7 +33,7 @@ spec p = do
       (BS.concat . LBS.toChunks $ AE.encode resource) `shouldEqual` (getBody response)
 
   describe "POST /spots" $
-    it "should create new Spot record" $ do
+    it "should create new Spot record" $ cleanup $ do
       let resource = Spot 1.2 1.3 "ABCDE"
       app    <- getApp p
       before <- runDB p $ P.count ([] :: [P.Filter Spot])
@@ -45,7 +42,7 @@ spec p = do
       before < after `shouldBe` True
 
   describe "PUT /spots/:id" $
-    it "should update existing record" $ do
+    it "should update existing record" $ cleanup $ do
       resourceId <- runDB p $ P.insert (Spot 1.2 1.3 "FOO")
       app    <- getApp p
       _      <- put app (BS.concat ["spots/", (BSC8.pack $ show resourceId)]) $ AE.encode (Spot 1.2 1.3 "BAR")
